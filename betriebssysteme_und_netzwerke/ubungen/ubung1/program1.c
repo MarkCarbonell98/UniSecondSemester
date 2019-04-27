c7790b67c2409fe59f3fa0ab43ae8a55217bb5a8
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-04-27 13:25:15 +0200

    added programs1 and 2

--- betriebssysteme_und_netzwerke/ubungen/ubung1/program1.c
+++ betriebssysteme_und_netzwerke/ubungen/ubung1/program1.c
@@ -1,11 +1,23 @@
-
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 //program 1
 int main(int argc, char* argv[]) {
     int i = 0;
-    if(fork() != 0) i++;
-    if(i != 0) fork();
-    if(fork() != 0 || i != 0) i++;
-    if(fork() == 0 && i == 0) fork();
+    pid_t pid = fork();
+    if(fork() != 0) i++,printf("after first i++ (fork() != 0) pid = %d, i = %d \n\n",pid, i); //passing from 
+    if(i != 0) {
+        printf(" before fork(), (i != 0) pid = %d, i = %d \n\n",pid, i);
+        fork();
+        printf(" after fork(), (i != 0) pid = %d, i = %d \n\n",pid, i); 
+    }
+    if(fork() != 0 || i != 0) i++, printf("after last i++ (fork() != 0 || i != 0) pid = %d, i = %d \n\n",pid, i);
+    if(fork() == 0 && i == 0) {
+        printf(" before fork(), (fork() == 0 && i == 0) pid = %d, i = %d \n\n",pid, i);
+        fork();
+        printf(" after fork(), (fork() == 0 && i == 0) pid = %d, i = %d \n\n",pid, i); 
+    }
     return 0;
 }
 
