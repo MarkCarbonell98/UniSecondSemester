10ee00065171142393797a64595ba6c1955f9948
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-05-05 09:08:56 +0200

    finished blatt ibn

--- betriebssysteme_und_netzwerke/ubungen/ubung2/aufgabe3/program2.c
+++ betriebssysteme_und_netzwerke/ubungen/ubung2/aufgabe3/program2.c
@@ -6,22 +6,21 @@
 
 int main(int argc, char* argv[]) {
     int i = 0;
-    printf("first fork pid = %d, i = %d \n", getpid(), i); //parent fork runs first
     if(fork() != 0) { // is it parent process ?
         i += 2;
-        printf("second pid = %d, i = %d \n",getpid(), i);
+        printf("parent process actualPid = %d, parentPid = %d, i = %d \n\n",getpid(), getppid(), i); 
     }
     if (fork() == 0) { // is it child process ?
         i -= 1;
-        printf("thrith pid = %d, i = %d \n",getpid(), i);
+        printf("child process actualPid = %d, parentPid = %d, i = %d \n\n",getpid(), getppid(), i); 
     }
     if (fork() != 0) { // is it parent process ?
         i -= 1;
-        printf("fourth pid = %d, i = %d \n", getpid(), i);
+        printf("parent process actualPid = %d, parentPid = %d, i = %d \n\n",getpid(), getppid(), i); 
     }
     if (fork() * i == 0) { // is it child process ?
         fork();
-        printf("fifth pid = %d, i = %d \n", getpid(), i);
+        printf(" child process before 3 levels of nesting actualPid = %d, parentPid = %d, i = %d \n\n",getpid(), getppid(), i); 
     }
     exit(0);
     return 0;
