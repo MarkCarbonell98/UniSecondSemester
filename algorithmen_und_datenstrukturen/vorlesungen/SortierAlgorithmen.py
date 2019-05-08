dd86625cf2f58679e263111e88ef9591279002d0
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-05-08 20:59:03 +0200

    added new sheets

--- algorithmen_und_datenstrukturen/vorlesungen/SortierAlgorithmen.py
+++ algorithmen_und_datenstrukturen/vorlesungen/SortierAlgorithmen.py
@@ -50,28 +50,40 @@ print("Insertion sort: ",testList)
 # mergesort und quicksort
 
 def merge(left, right):
-    res = []
-    i, j = 0,0
+    res = []     # the result array is initially empty
+    i, j = 0, 0  # i and j always point to the first element in left and 
+                #  right that has not yet been processed (= added to res)
     while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            res.append(left[i])
-            i += 1
+        if left[i] <= right[j]:    # compare the first unprocessed elements of left and right
+            res.append(left[i])   # the smallest remaining element is in left => add to res
+            i += 1                # go to next element in left
         else:
-            res.append(right[j])
-            j += 1
-    res += left[i : len(left)] + right[j : len(right)]
+            res.append(right[j])  # the smallest remaining element is in right => add to res
+            j += 1                # go to next element in right
+    # either left or right has been used up, add the remaining elements of the other
+    while i < len(left):
+        res.append(left[i])
+        i += 1
+    while j < len(right):
+        res.append(right[j])
+        j += 1
+    # the latter two while loops can be abbreviated by Python's slice syntax
+    # res += left[i:len(left)] + right[j:len(right)]
+    # (syntax: array[m:n] returns the subarray from index m (inclusive) to index n (exclusive))
     return res
 
-def mergeSort(a):
-    n = len(a)
-    if n <= 1:
-        return a
+
+def mergeSort(a):  # sort 'a' out-of-place (i.e. a new sorted array is returned, 'a' is unchanged)
+    N = len(a)     # number of elements
+    if N <= 1:
+        return a   # arrays with 0 or 1 elements are already sorted => stop recursion
     else:
-        left = a[0 : n//2] # floor(n/2)
-        right = a[n//2 : n]
-        leftSorted = mergeSort(left)
-        rightSorted = mergeSort(right)
-        return merge(leftSorted, rightSorted)
+        left  = a[0:N//2]   # slice syntax: left subarray
+                            # N//2 is Python's 'floor division'
+        right = a[N//2:N]   # slice syntax: right subarray
+        leftSorted  = mergeSort(left)  # recursively sort left and ...
+        rightSorted = mergeSort(right) # ... right subarrays
+        return merge(leftSorted, rightSorted)  # merge the sorted parts and return result
 
 testList = random.sample(range(101), 50)
 print("MergeSort", mergeSort(testList))
@@ -83,30 +95,38 @@ print("MergeSort", mergeSort(testList))
 # ors gint
 # ginorst
 
-def partition(a, l, r): #left and right pointers. a is the array
-    pivot = a[r]
-    i = l
-    j = r - 1
+def partition(a, l, r):
+    pivot = a[r]     # Pivot-Element. Hier wird willkürlich das letzte Element verwendet.
+    i  = l           # i und j sind Laufvariablen
+    j  = r - 1
     while True:
         while i < r and a[i] <= pivot:
-            i += 1
+            i += 1               # finde von links das erste Element > pivot
         while j > l and a[j] >= pivot:
-            i -= 1
-        if i < j:
-            a[i], a[j] = a[j], a[i] #swap
-        else:
-            break
-        a[r] = pivot
-        return i
+            j -= 1               # finde von rechts das ersten Element < pivot
+                                # (von links gesehen ist dies das letzte Element < pivot)
+        if i < j:                # a[i] und a[j] sind beide auf der falschen Seite des Pivot
+            a[i], a[j] = a[j], a[i]  # => vertausche sie
+        else:                    # alle Elemente sind auf der richtigen Seite
+            break                # => Schleife beenden      
+    a[r] = a[i]                  # schaffe eine Lücke für das Pivot
+                                # wegen a[i] >= pivot gehört das bisherige a[i] auf die rechte Seite
+                                # wegen a[j] <= pivot und i >= j ist i die richtige Position
+    a[i] = pivot                 # bringe das Pivot an seine endgültige Position i
+    return i                     # gib die engültige Position des Pivots zurück
 
-def quickSortImpl(a, l, r):
-    if r > l:
-        k = partition(a,l,r)
-        quickSortImpl(a,l, k - 1)
-        quickSortImpl(a, k + 1, r)
+def quickSortImpl(a, l, r): 
+    """a ist das zu sortierende Array, 
+    l und r sind die linke und rechte Grenze (inklusive) des zu sortierenden Bereichs"""
 
-def quickSort(a):
-    quickSortImpl(a, 0, len(a) - 1)
+    if r > l:                     # Rekursionsabschluss: wenn r <= l, enthält der Bereich höchstens ein Element
+                                # und muss nicht mehr sortiert werden
+        k = partition(a, l, r)    # k ist der Index des sog. Pivot-Elements (s. u.)
+        quickSortImpl(a, l, k-1)  # rekursives Sortieren des linken ... 
+        quickSortImpl(a, k+1, r)  # ... und rechten Teilarrays
+
+def quickSort(a):                 # sortiere Array 'a' in-place
+    quickSortImpl(a, 0, len(a)-1) # rufe den eigentlichen Algorithmus mit Arraygrenzen auf  
 
 testList = random.sample(range(101), 50)
 print(quickSort(testList))
