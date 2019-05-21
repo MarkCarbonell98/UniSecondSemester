f96de1f2a77ceacfc62f84949bb143b60262fee3
MarkCarbonell98 <markcarbonell2017@outlook.com>
2019-05-21 10:18:50 +0200

    added aufgabe1test.c

--- betriebssysteme_und_netzwerke/ubungen/ubung4/aufgabe1test.c
+++ betriebssysteme_und_netzwerke/ubungen/ubung4/aufgabe1test.c
@@ -2,32 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#define NUM_THREADS 5
+// #define NUM_THREADS 1
+// #define NUM_THREADS 5
+// #define NUM_THREADS 10
+// #define NUM_THREADS 100
 
-void * TaskCode(void * argument) {
+pthread_mutex_t mutex;
+pthread_cond_t parent_cond, child_cond;
+
+int childInnerCounter = 0, i = 0;
+
+
+void * childThreadFunction(void * argument) {
     int tid;
     tid = *((int *) argument);
-    printf("I am the number %d \n", tid);
+    ++i;
+    while(1) {
+        pthread_mutex_lock(&mutex);
+        ++childInnerCounter;
+        pthread_mutex_unlock(&mutex);
+    }
     return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
-    pthread_t threads[NUM_THREADS];
-    int thread_args[NUM_THREADS];
-    int rc;
-    int i;
-    for(i = 0; i < NUM_THREADS; ++i) {
-        thread_args[i] = i;
-        printf("In main: creating thread #%d \n", i);
-        rc = pthread_create(&threads[i], NULL, TaskCode, (void *) &thread_args[i]);
-        assert( 0 == rc);
-    }
+    if(argc == 2) {
+        pthread_mutex_init(&mutex, 0);
+        char * input = argv[1];
+        int n = atoi(input);
+        pthread_t threads[n];
+        int thread_args[n];
+        int rc;
+        int i;
+        for(i = 0; i < n; ++i) {
+            thread_args[i] = i;
+            printf("In main: creating thread #%d \n", i);
+            rc = pthread_create(&threads[i], NULL, childThreadFunction, (void *) &thread_args[i]);
+            assert( 0 == rc);
+        }
 
-    for(i = 0; i <NUM_THREADS; ++i) {
-        rc = pthread_join(threads[i], NULL);
-        assert(0 == rc);
+        wait(1);
+        childInnerCounter = 0;
+        sleep(3);
+
+        printf("There were %d threads created, and the child thread counter is %d \n", i, childInnerCounter);
+
+        pthread_mutex_destroy(&mutex);
+        exit(EXIT_SUCCESS);
+        return 0;
+    } else {
+        printf("You must insert the number of threads n to start the program, try again! \n");
+        exit(EXIT_FAILURE);
+        return 0;
     }
-    exit(EXIT_SUCCESS);
-    return 0;
 }
