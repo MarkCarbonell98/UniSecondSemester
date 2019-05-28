0617675ffc5ae4eaa7ea271851ed1404899d6fdc
LukasBet <50401917+LukasBet@users.noreply.github.com>
2019-05-28 18:19:59 +0200

    Update searchTreeTest.py

--- algorithmen_und_datenstrukturen/ubungen/ubung5/searchTreeTest.py
+++ algorithmen_und_datenstrukturen/ubungen/ubung5/searchTreeTest.py
@@ -8,22 +8,34 @@ class TestSearchTree(unittest.TestCase):
 
     def setUp(self):
         tree = SearchTree()
-        for someNumber in range(random.randint(1,20)):
-            tree.insert(random.randint(-1000, 1000), ''.join(random.choice(string.ascii_lowercase) for i in range(random.randint(1, 20))))
+        someNumber = random.randin(1,20)
+        for i in range(someNumber):
+            tree.insert(random.randint(-1000, 1000), ''.join(random.choice(string.ascii_lowercase) for j in range(someNumber)))
         self.tree = tree
 
-    def isTreeValid(self, tree):
-        # todo
+    def test_Tree_is_root(self):
+        
         print(self.tree.BFTraversal())
         tree = SearchTree()
         assert tree.left == None and tree.right == None
         
 
-    def testStartingValues(self):   
+    def test_Starting_Values(self):   
         
-        #todo
         print(self.tree.BFTraversal())
+        tree = SearchTree(1,1)
+        assert tree.key == 1 and tree.value == 1
+        
 
+    def test_tree_length(self,tree):
+        
+        print(self.tree.BFTraversal())
+        assert tree.size == someNumber
+        
+    
+    def test_minimal_node(self):
+        print(self.tree.BFTraversal)
+        assert 
 
 
 if __name__ == '__main__':
