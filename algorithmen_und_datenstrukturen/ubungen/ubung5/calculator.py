756551bc3deff3278cb7a241537fa1ac0df49334
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-05-28 08:59:01 +0200

    working on calculator.py

--- algorithmen_und_datenstrukturen/ubungen/ubung5/calculator.py
+++ algorithmen_und_datenstrukturen/ubungen/ubung5/calculator.py
@@ -1,47 +1,349 @@
-
 class Number:
-    def __init__(self, number):
-        self.number = number
+    def __init__(self, Number):
+        self.Number = number
 
 class Operator:
-    def __init__(self, operator, precedence, associativity):
+    def __init__(self, left, operator, right):
         self.operator = operator
-        self.left = self.right = None
-        self.precedence = precedence
-        self.associativity = associativity
+        self.left = left,
+        self.right = right
+
+def isParenthesis(char):
+    return char == '(' or char == ')'
+
+def isNumber(char):
+    return char.isdigit()
+
+def isOperator(char):
+    return char == '*' or char == '/' or char == '+' or char == '-'
+
+# compares if the op1 has greater precence than the op2
+def hasGreaterPrecedence(op1, op2):
+    if(op1 == '*' or op1 == '/' and (op2 == '+' or op2 == '-')):
+        return True
+    return False
+
+def hasEqualPrecedence(op1, op2):
+    if(op1 == '+' or op1 == '-' and (op2 == '+' or op2 == '-')):
+        return True
+    elif(op1 == '*' or op1 == '/' and (op2 == '*' or op2 == '/')):
+        return True
+    return False
+
 
 def parse(s):
-    print()
-    '''
-        while there are tokens in S
+    
+    expr = list(s)
+    print
+    # root = Operator(None, None, None)
+    numberStack, operatorStack = [], []
+    for i in range(len(expr)):
+        top = len(operatorStack) - 1
+        if isNumber(expr[i]):
+            numberStack.append(expr[i])
+        
+        if isParenthesis(expr[i]):
+            operatorStack.append(expr[i])
+
+        if isOperator(expr[i]):
             
-            if token is a number, then push it to the number stack
+            while(isParenthesis(operatorStack[top]) or hasGreaterPrecedence(operatorStack[top], expr[i]) or hasEqualPrecedence(operatorStack[top], expr[i]) and operatorStack[top] != '('):
+                numberStack.append(operatorStack.pop(top))
+            operatorStack.append(expr[i])
 
-            if token is an parenthesis then push it to the operator stack
+        if expr[i] == '(':
+            operatorStack.append(expr[i])
 
-            if token is an operator then
+        if expr[i] == ')':
+            while(operatorStack[top] != '('):
+                numberStack.append(operatorStack.pop(top))
+            if operatorStack[top] == '(':
+                operatorStack.pop(top)
+    if(not len(expr)):
+        while(top != 0):
+            if(isParenthesis(operatorStack[top])): raise SyntaxError("Invalid Syntax on this expression")
+            numberStack.append(operatorStack.pop(top))
+    return numberStack
+    
+    
 
-                while(there is a parenthesis at the top of the op. stack || there is op. at the top of op. stack with greater precedence || the op. at the op of op has equal precendence and is left associative && the op at top of op. stack is not a left parenthesis) 
-                    pop operators from op. stack to the number stack
-                push op. to op. stack
+parse('2+2');
 
-            if char is a left parenthesis, then
-                push it to op. stack
-            if char is a right paren, then
-                while the op. at op stack is not a left paren
-                    pop op. from op.stack into the number stack
-                if left paren at top of op. stack
-                    pop op. from op. stack and erase it
-        if no more char to read
-            while op. tokens in op stack stack
-                pop op. from op. stack and push to number stack
+            
+#     '''
+#         while there are tokens in S
+            
+#             if token is a number, then push it to the number stack
+
+#             if token is an parenthesis then push it to the operator stack
+
+#             if token is an operator then
+
+#                 while(there is a parenthesis at the top of the op. stack || there is op. at the top of op. stack with greater precedence || the op. at the op of op has equal precendence and is left associative && the op at top of op. stack is not a left parenthesis) 
+#                     pop operators from op. stack to the number stack
+#                 push op. to op. stack
+
+#             if char is a left parenthesis, then
+#                 push it to op. stack
+#             if char is a right paren, then
+#                 while the op. at op stack is not a left paren
+#                     pop op. from op.stack into the number stack
+#                 if left paren at top of op. stack
+#                     pop op. from op. stack and erase it
+#         if no more char to read
+#             while op. tokens in op stack stack
+#                 pop op. from op. stack and push to number stack
                 
-        exit
+#         exit
                 
 
 
-    '''
+#     '''
 
+
+# INTEGER, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, EOF = (
+#     'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', '(', ')', 'EOF'
+# )
+
+
+# class Token(object):
+#     def __init__(self, type, value):
+#         self.type = type
+#         self.value = value
+
+#     def __str__(self):
+#         """String representation of the class instance.
+
+#         Examples:
+#             Token(INTEGER, 3)
+#             Token(PLUS, '+')
+#             Token(MUL, '*')
+#         """
+#         return f'Token({self.type}, {repr(self.value)})'
+
+#     def __repr__(self):
+#         return self.__str__()
+
+
+# class Lexer(object):
+#     def __init__(self, text):
+#         # client string input, e.g. "4 + 2 * 3 - 6 / 2"
+#         self.text = text
+#         # self.pos is an index into self.text
+#         self.pos = 0
+#         self.current_char = self.text[self.pos]
+
+#     def error(self):
+#         raise Exception('Invalid character')
+
+#     def advance(self):
+#         """Advance the `pos` pointer and set the `current_char` variable."""
+#         self.pos += 1
+#         if self.pos > len(self.text) - 1:
+#             self.current_char = None  # Indicates end of input
+#         else:
+#             self.current_char = self.text[self.pos]
+
+#     def skip_whitespace(self):
+#         while self.current_char is not None and self.current_char.isspace():
+#             self.advance()
+
+#     def integer(self):
+#         """Return a (multidigit) integer consumed from the input."""
+#         result = ''
+#         while self.current_char is not None and self.current_char.isdigit():
+#             result += self.current_char
+#             self.advance()
+#         return int(result)
+
+#     def get_next_token(self):
+#         """Lexical analyzer (also known as scanner or tokenizer)
+
+#         This method is responsible for breaking a sentence
+#         apart into tokens. One token at a time.
+#         """
+#         while self.current_char is not None:
+
+#             if self.current_char.isspace():
+#                 self.skip_whitespace()
+#                 continue
+
+#             if self.current_char.isdigit():
+#                 return Token(INTEGER, self.integer())
+
+#             if self.current_char == '+':
+#                 self.advance()
+#                 return Token(PLUS, '+')
+
+#             if self.current_char == '-':
+#                 self.advance()
+#                 return Token(MINUS, '-')
+
+#             if self.current_char == '*':
+#                 self.advance()
+#                 return Token(MUL, '*')
+
+#             if self.current_char == '/':
+#                 self.advance()
+#                 return Token(DIV, '/')
+
+#             if self.current_char == '(':
+#                 self.advance()
+#                 return Token(LPAREN, '(')
+
+#             if self.current_char == ')':
+#                 self.advance()
+#                 return Token(RPAREN, ')')
+
+#             self.error()
+
+#         return Token(EOF, None)
+
+
+# class AST(object):
+#     pass
+
+
+# class Operator(AST):
+#     def __init__(self, left, op, right):
+#         self.left = left
+#         self.token = self.op = op
+#         self.right = right
+
+
+# class Number(AST):
+#     def __init__(self, token):
+#         self.token = token
+#         self.value = token.value
+
+
+# class Parser(object):
+#     def __init__(self, lexer):
+#         self.lexer = lexer
+#         # set current token to the first token taken from the input
+#         self.current_token = self.lexer.get_next_token()
+
+#     def error(self):
+#         raise Exception('Invalid syntax')
+
+#     def eat(self, token_type):
+#         # compare the current token type with the passed token
+#         # type and if they match then "eat" the current token
+#         # and assign the next token to the self.current_token,
+#         # otherwise raise an exception.
+#         if self.current_token.type == token_type:
+#             self.current_token = self.lexer.get_next_token()
+#         else:
+#             self.error()
+
+#     def factor(self):
+#         """factor : INTEGER | LPAREN expr RPAREN"""
+#         token = self.current_token
+#         if token.type == INTEGER:
+#             self.eat(INTEGER)
+#             return Number(token)
+#         elif token.type == LPAREN:
+#             self.eat(LPAREN)
+#             node = self.expr()
+#             self.eat(RPAREN)
+#             return node
+
+#     def term(self):
+#         """term : factor ((MUL | DIV) factor)*"""
+#         node = self.factor()
+
+#         while self.current_token.type in (MUL, DIV):
+#             token = self.current_token
+#             if token.type == MUL:
+#                 self.eat(MUL)
+#             elif token.type == DIV:
+#                 self.eat(DIV)
+
+#             node = Operator(left=node, op=token, right=self.factor())
+
+#         return node
+
+#     def expr(self):
+#         """
+#         expr   : term ((PLUS | MINUS) term)*
+#         term   : factor ((MUL | DIV) factor)*
+#         factor : INTEGER | LPAREN expr RPAREN
+#         """
+#         node = self.term()
+
+#         while self.current_token.type in (PLUS, MINUS):
+#             token = self.current_token
+#             if token.type == PLUS:
+#                 self.eat(PLUS)
+#             elif token.type == MINUS:
+#                 self.eat(MINUS)
+
+#             node = Operator(left=node, op=token, right=self.term())
+
+#         return node
+
+#     def parse(self):
+#         return self.expr()
+
+
+# ###############################################################################
+# #                                                                             #
+# #  INTERPRETER                                                                #
+# #                                                                             #
+# ###############################################################################
+
+# class NodeVisitor(object):
+#     def visit(self, node):
+#         method_name = 'visit_' + type(node).__name__
+#         visitor = getattr(self, method_name, self.generic_visit)
+#         return visitor(node)
+
+#     def generic_visit(self, node):
+#         raise Exception('No visit_{} method'.format(type(node).__name__))
+
+
+# class Interpreter(NodeVisitor):
+#     def __init__(self, parser):
+#         self.parser = parser
+
+#     def visit_Operator(self, node):
+#         if node.op.type == PLUS:
+#             return self.visit(node.left) + self.visit(node.right)
+#         elif node.op.type == MINUS:
+#             return self.visit(node.left) - self.visit(node.right)
+#         elif node.op.type == MUL:
+#             return self.visit(node.left) * self.visit(node.right)
+#         elif node.op.type == DIV:
+#             return self.visit(node.left) / self.visit(node.right)
+
+#     def visit_Number(self, node):
+#         return node.value
+
+#     def interpret(self):
+#         tree = self.parser.parse()
+#         return self.visit(tree)
+
+# def main():
+#     while True:
+#         try:
+#             try:
+#                 text = raw_input('spi> ')
+#             except NameError:  # Python3
+#                 text = input('spi> ')
+#         except EOFError:
+#             break
+#         if not text:
+#             continue
+
+#         lexer = Lexer(text)
+#         parser = Parser(lexer)
+#         interpreter = Interpreter(parser)
+#         result = interpreter.interpret()
+#         print(result)
+
+
+# if __name__ == '__main__':
+#     main()
 
 
 
