4a03de68ea60a71f1f657a83b49932614833e916
LukasBet <50401917+LukasBet@users.noreply.github.com>
2019-05-29 09:43:27 +0200

    Update searchTreeTest.py

--- algorithmen_und_datenstrukturen/ubungen/ubung5/searchTreeTest.py
+++ algorithmen_und_datenstrukturen/ubungen/ubung5/searchTreeTest.py
@@ -30,13 +30,19 @@ class TestSearchTree(unittest.TestCase):
     def test_tree_length(self,tree):
         
         print(self.tree.BFTraversal())
-        assert tree.size == someNumber
+        assert self.tree.size == someNumber
         
     
-    def test_minimal_node(self):
+    def test_insert_and_remove(self):
         print(self.tree.BFTraversal)
-        assert 
+        tree = SearchTree()
+        tree.insert(tree,1,'teststring')
+        assert tree.find(tree,1) == 1
+        
+        tree.remove(tree,1,'teststring')
+        assert tree.find(tree,1) == False
 
+    
 
 if __name__ == '__main__':
     unittest.main()
