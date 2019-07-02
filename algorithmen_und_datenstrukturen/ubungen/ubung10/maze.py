3fc43cc7b1986a75ec6cb6f09992acbf43f18230
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-07-02 16:48:57 +0200

    added aufgabe 2b

--- algorithmen_und_datenstrukturen/ubungen/ubung10/maze.py
+++ algorithmen_und_datenstrukturen/ubungen/ubung10/maze.py
@@ -1,17 +1,5 @@
 # Aufgabe 1a
 
-def dfs(graph, startnode):
-    visited = [False]*len(graph)  
-    def visit(node):            
-        if not visited[node]: 
-            visited[node] = True 
-            print(node)          
-            for neighbor in graph[node]:   
-                visit(neighbor)
-    visit(startnode)
-
-# alg aus der vorlesung... 
-
 graph = [[1], [2,3], [1], [4,5], [3], [3,6,7], [5], [5,8,10], [7,9,10], [8], [7,8, 11], [12,13], [11] ,[11, 14, 15], [13], [13]]
 
 for arr in graph:
@@ -19,16 +7,17 @@ for arr in graph:
 
 print("Adjazenzlisten: ", graph)
 
+# aufgabe 1b
 def way_out(graph, startnode, targetnode):
     visited = [None]*len(graph)
-    deadEnds = []
+    deadEnds = set()
     targetReached = [False]
     def visit(node):
         if node == targetnode:
             targetReached[0] = True
         isDeadEnd = len(graph[node]) == 1 and node != targetnode and node != startnode
         if isDeadEnd:
-            deadEnds.append(node)
+            deadEnds.add(node)
             print(node, "dead end")
         else:
             print(node)
@@ -44,9 +33,10 @@ def way_out(graph, startnode, targetnode):
         print(f"the algorithm found {len(deadEnds)} dead ends on the way")
     else:
         print("target out of range")
+    return deadEnds, targetReached
 
 # der anfang und ende des Graphes werden nicht als dead ends betrachtet
-way_out(graph, 0, 0)
+way_out(graph, 0, 15)
 
 def way_out_stack(graph, startnode, endnode):
     stack = [startnode]
@@ -76,7 +66,32 @@ def way_out_stack(graph, startnode, endnode):
         print(f"the algorithm found {len(deadEnds)} dead ends on the way")
     else:
         print("target out of range")
+    return deadEnds, targetReached
 
 way_out_stack(graph, 0, 15)
+
+import unittest
+
+class TestWayOut(unittest.TestCase):
+    def setUp(self):
+        self.graph = graph
+        self.possibleSearches = [[0,15], [1,15], [0,0], [4,15], [1,5], [15,15]]
+
+    # somit laufen way_out und way_out_stack gleich
+    def test_both_way_outs(self):
+        for i in range(len(self.possibleSearches)):
+            actualStartAndEndNodes = self.possibleSearches[i]
+            deadEnds, targetReached = way_out(self.graph, actualStartAndEndNodes[0], actualStartAndEndNodes[1])
+            deadEndsStack, targetReachedStack = way_out_stack(self.graph, actualStartAndEndNodes[0], actualStartAndEndNodes[1])
+            self.assertEqual(len(deadEndsStack), len(deadEnds))
+            self.assertEqual(len(targetReached), len(targetReachedStack))
+            self.assertEqual(deadEnds, deadEndsStack)
+            self.assertEqual(targetReached, targetReachedStack)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
---
+++ algorithmen_und_datenstrukturen/ubungen/ubung10/schiebepuzzle.py
@@ -0,0 +1,110 @@
+
+# aufgabe a
+p = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,' ']
+
+def inZeile(a,b):
+    if b > 9 and b != ' ': 
+        print(a, end=' ')
+    else:
+        print(a, end='  ')
+
+def anfangderZeile(a,b):
+    if a == ' ':
+        if b > 9:
+            print(' ', a, end=' ')
+        else:
+            print(' ', a, end='  ')
+    elif b == ' ':
+        if a > 9:
+            print(' ', a, end='  ')
+        else:
+            print('  ', a, end= '  ')
+    else:
+        if a > 9:
+            if b > 9: 
+                print(' ',a, end=' ')
+            else:
+                print(' ',a, end='  ')
+        else:
+            if b > 9: 
+                print('  ',a, end=' ')
+            else:
+                print('  ',a, end='  ')
+            
+def print_pos(p):
+    for i in range(len(p)):
+        if (i-3)%4 == 0:
+            print(p[i], end='\n')
+        elif i%4 == 0:
+            if p[i] != ' ' and p[i+1] != ' ':
+                anfangderZeile(p[i], p[i+1])
+            else:
+                if p[i] == ' ':
+                    if p[i+1] > 9:
+                        print('  ',p[i], end=' ')
+                    else:
+                        print('  ',p[i], end='  ')
+                else:
+                    if p[i] > 9:
+                        print(' ', p[i], end='  ')
+                    else:
+                        print('  ', p[i], end='  ')
+        else:
+            if i == 14: 
+                print(p[i], end=' ')
+            else:
+                if p[i+1] != ' ':
+                    inZeile(p[i], p[i+1])
+                else:
+                    print(p[i], end='  ')
+# print_pos(p)
+
+# aufgabe b
+import random
+
+# topologische Sortierung hat O(E + V) komplexitaet. Also gesamte Komplexitaet ist O(E) 
+
+def swap(arr,a,b):
+    arr[a], arr[b] = arr[b], arr[a]
+
+def shuffle_pos(A, N):
+    lastMove = None
+    puzzleLength = len(A)
+    sideLength = int(puzzleLength/4)
+    for i in range(N):
+        holePosition = A.index(' ')
+        move = random.randint(0,3)
+        if move == 0 and lastMove != 2 and (holePosition + 1) + sideLength <= puzzleLength: # up
+            swap(A, holePosition, holePosition + sideLength)
+        elif move == 2 and lastMove != 0 and (holePosition + 1) - sideLength > 0:
+            swap(A, holePosition, holePosition - sideLength)
+        elif move == 3 and lastMove != 1 and ((holePosition) % sideLength) != 0:
+            swap(A, holePosition, holePosition-1)
+        elif move == 1 and lastMove != 3 and ((holePosition + 1) % sideLength) != 0:
+            swap(A, holePosition, holePosition+1)
+        lastMove = move
+
+shuffle_pos(p, 100)
+print_pos(p)
+
+# aufgabe c
+
+from collections import deque
+
+def solve_bfs(p, maxlevel):
+    parents = [None]*len(graph)            
+    parents[startnode] = startnode         
+    q = deque()                            # Queue für die zu besuchenden Knoten
+    q.append(startnode) 
+    counter = 0;                   # Startknoten in die Queue einfügen
+    
+    while len(q) > 0:
+        if counter == maxlevel: break                      # solange noch Knoten zu bearbeiten sind
+        counter += 1
+        node = q.popleft()                 # Knoten aus der Queue nehmen (first in - first out)
+        print(node)                        # den Knoten bearbeiten (hier: Knotennummer drucken)
+        for neighbor in graph[node]:       # die Nachbarn expandieren
+            if parents[neighbor] is None:  # Nachbar wurde noch nicht besucht
+                parents[neighbor] = node   
+                q.append(neighbor)         #    und in die Queue aufnehmen
+
