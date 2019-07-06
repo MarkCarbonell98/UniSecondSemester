8c0e3f3c243b28791d83b415d9834a6950a72f93
Marcos Carbonell <markcarbonell2017@outlook.com>
2019-07-06 16:32:20 +0200

    added createGraph function

--- algorithmen_und_datenstrukturen/ubungen/ubung11/graph.py
+++ algorithmen_und_datenstrukturen/ubungen/ubung11/graph.py
@@ -1,31 +1,14 @@
 from pgm import readPGM, writePGM
 width, height, data = readPGM("cells.pgm")
 
-# Aufgabe 1
-backgroundThreshold = 60
-def createMastk(width, height, data, threshold):
-    mask = [0]*len(data)
-    for x in range(width):
-        for y in range(height):
-            if data[x + y*width] >= threshold: 
-                mask[x+y*width] = data[x + y * width]
-    return mask
-
-mask = createMastk(width, height, data, backgroundThreshold)
-# writePGM(width, height, mask, "mask.pgm")
-
-
 def createGraph(width, height, mask):
     adjacencyList = []
     leftPixel = 0
     rightPixel = 0
     lowerPixel = 0
     upperPixel = 0
-    currentPixel = 0
-
     for x in range(width):
         for y in range(height):
-            currentPixel = mask[x + y * width]
             newVertex = []
             try:
                 leftPixel = mask[(x - 1) + y * width]
@@ -43,19 +26,19 @@ def createGraph(width, height, mask):
                 upperPixel = mask[x + (y + 1) * width]
             except IndexError:
                 upperPixel = 0
-
+                
             if x == 0 and y == 0:
-                newVertex.append(lowerPixel)
                 newVertex.append(rightPixel)
-            elif x == 0 and y == height - 1:
                 newVertex.append(upperPixel)
+            elif x == 0 and y == height - 1:
+                newVertex.append(lowerPixel)
                 newVertex.append(rightPixel)
             elif x == width - 1 and y == 0:
                 newVertex.append(lowerPixel)
                 newVertex.append(leftPixel)
-            elif x == width - 1 and y == height - 1:
-                newVertex.append(upperPixel)
+            elif x == width -1 and y == height -1:
                 newVertex.append(leftPixel)
+                newVertex.append(upperPixel)
             elif x == 0 and y > 0 and y < height-1:
                 newVertex.append(upperPixel)
                 newVertex.append(lowerPixel)
@@ -64,29 +47,21 @@ def createGraph(width, height, mask):
                 newVertex.append(upperPixel)
                 newVertex.append(lowerPixel)
                 newVertex.append(leftPixel)
-            elif x > 0 and x < width - 1 and y == 0:
-                newVertex.append(leftPixel)
-                newVertex.append(rightPixel)
-                newVertex.append(lowerPixel)
-            elif x > 0 and x < width - 1 and y == height - 1:
+            elif x > 0 and x < width -1 and y == 0:
                 newVertex.append(leftPixel)
                 newVertex.append(rightPixel)
                 newVertex.append(upperPixel)
+            elif x > 0 and x < width -1 and y == height -1:
+                newVertex.append(leftPixel)
+                newVertex.append(rightPixel)
+                newVertex.append(lowerPixel)
             else:
                 newVertex.append(leftPixel)
                 newVertex.append(rightPixel)
                 newVertex.append(upperPixel)
                 newVertex.append(lowerPixel)
-
-            for weight in list(newVertex):
-                if weight < backgroundThreshold and currentPixel >= backgroundThreshold or (weight >= backgroundThreshold and  currentPixel < backgroundThreshold):
-                    newVertex.remove(weight)
-            if len(newVertex):
-                adjacencyList.append(newVertex)
+            adjacencyList.append(newVertex)
     return adjacencyList
 
-
-graph = createGraph(width, height, mask)
-
-
-
+graph = createGraph(width, height, data)
+print(graph)
